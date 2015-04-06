03c5881461e1006dfa488253eb1ce7699c8125a2
marcosrseki <marcos.seki@yahoo.com.br>
2015-04-06 11:15:20 -0300

    Controller loja

---
+++ Aula3/Controllers/lojaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aula3.Controllers
+{
+    public class lojaController : Controller
+    {
+        // GET: loja
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
